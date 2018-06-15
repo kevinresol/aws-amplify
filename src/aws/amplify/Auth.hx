abfae0823cb99c2e58b6b5e04332c0183b38c308
Kevin Leung <kevinresol@gmail.com>
2018-06-15 11:53:06 +0800

    update

--- src/aws/amplify/Auth.hx
+++ src/aws/amplify/Auth.hx
@@ -12,7 +12,7 @@ extern class Auth {
 	static function signIn(username:String, password:String):Promise<Any>;
 	static function signOut():Promise<Any>;
 	
-	static function signUp(username:String, password:String):Promise<Any>;
+	static function signUp(info:SignUpInfo):Promise<Any>;
 	static function confirmSignUp(username:String, code:String):Promise<Any>;
 	static function resendSignUp(username:String):Promise<Any>;
 	
@@ -22,12 +22,24 @@ extern class Auth {
 	static function forgotPasswordSubmit(username:String, code:String, password:String):Promise<Any>;
 	
 	static function currentAuthenticatedUser():Promise<CognitoUser>;
-	static function currentSession():Promise<{idToken:{jwtToken:String}}>;
+	static function currentSession():Promise<Sessiond>;
 	static function currentUserInfo():Promise<UserInfo>;
 	static function currentUserPoolUser():Promise<CognitoUser>;
 }
 
 extern class CognitoUser {}
+
+typedef Session = {
+	idToken:{
+		jwtToken:String,
+	},
+}
+
+typedef SignUpInfo = {
+	username:String,
+	password:String,
+	attributes:DynamicAccess<String>,
+}
 
 typedef UserInfo = {
 	id:String,
