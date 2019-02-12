7ebfc2b1ab2fb2970cdd74193765c22832210d17
Kevin Leung <kevinresol@gmail.com>
2019-02-12 19:39:13 +0800

    Update some types

--- src/aws/amplify/Auth.hx
+++ src/aws/amplify/Auth.hx
@@ -9,15 +9,15 @@ import js.Promise;
 @:jsRequire('aws-amplify', 'Auth')
 #end
 extern class Auth {
-	static function signIn(username:String, password:String):Promise<Any>;
+	static function signIn(username:String, password:String):Promise<CognitoUser>;
 	static function signOut():Promise<Any>;
 	
 	static function signUp(info:SignUpInfo):Promise<Any>;
 	static function confirmSignUp(username:String, code:String):Promise<Any>;
 	static function resendSignUp(username:String):Promise<Any>;
 	
-	static function completeNewPassword(user:CognitoUser, password:String):Promise<Any>;
-	static function changePassword(user:CognitoUser, oldPassword:String, newPassword:String):Promise<Any>;
+	static function completeNewPassword(user:CognitoUser, password:String):Promise<CognitoUser>;
+	static function changePassword(user:CognitoUser, oldPassword:String, newPassword:String):Promise<String>;
 	static function forgotPassword(username:String):Promise<Any>;
 	static function forgotPasswordSubmit(username:String, code:String, password:String):Promise<Any>;
 	
@@ -27,7 +27,31 @@ extern class Auth {
 	static function currentUserPoolUser():Promise<CognitoUser>;
 }
 
-extern class CognitoUser {}
+typedef CognitoUser = {
+	username:String,
+	pool:CognitoUserPool, 
+	Session:String,
+	client:Client, 
+	signInUserSession:Dynamic, 
+	authenticationFlowType:String,
+	challengeName:String,
+	challengeParam:{
+		userAttributes:DynamicAccess<String>, 
+		requiredAttributes:Array<Dynamic>,
+	}
+}
+
+typedef Client = {
+	endpoint:String,
+	userAgent:String,
+}
+
+typedef CognitoUserPool = {
+	userPoolId:String,
+	clientId:String,
+	client:Client, 
+	advancedSecurityDataCollectionFlag:Bool,
+}
 
 typedef Session = {
 	idToken:{
