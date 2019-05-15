8a77fcfbcd9aad4572f4c47cf0705f4c5ea32f0b
Kevin Leung <kevinresol@gmail.com>
2019-05-15 13:37:41 +0800

    Revert Hub event handler type for now

--- src/aws/amplify/Hub.hx
+++ src/aws/amplify/Hub.hx
@@ -1,14 +1,12 @@
 package aws.amplify;
 
-import js.Promise;
-
 #if amplify_global
 @:native('window["aws-amplify"].Hub')
 #else
 @:jsRequire('aws-amplify', 'Hub')
 #end
 extern class Hub {
-	static function listen(name:String, f:Capsule->Void):Void;
+	static function listen(name:String, f:{onHubCapsule:Capsule->Void}):Void;
 }
 
 typedef Capsule = {
